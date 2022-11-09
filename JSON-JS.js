d06d5107355403055ce2fe5611f1ad3dc33f5a6a
Emmet <59771714+Firelikesdemon@users.noreply.github.com>
2022-11-09 00:00:32 +0000

    Backup

--- JSON-JS.js
+++ JSON-JS.js
@@ -1,14 +1,12 @@
 //Below converts the displayed messages on screen to a JSON file. It may not be specifically JavaScript to JSON, it may be HTML to JavaScript if it is easier to implement.
-function Js2Json(
+function Export() {
+  let test = '{ "List" : [' +
+'{ "nickname":"" , "Message":"" }]}';
 
-
-
-	
-)
+}
 //Below convers the uploaded JSON File from the user, checks the ID inside the JSON to confirm it was created with this site, and if it is valid, imports the messages inside back onto the screen. Like in the previous function, it may be HTML to JavaScript if it is easier to implement.
-function Json2Js(
-
-
+function Import() {
+	
 
 	
-)
+}
--- replit.nix
+++ replit.nix
@@ -1,9 +1,11 @@
 { pkgs }: {
   deps = [
+    pkgs.apacheHttpdPackages.php
     pkgs.sysvinit
     pkgs.root5
     pkgs.index-fm
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server
+		pkgs.php74
   ];
 }
